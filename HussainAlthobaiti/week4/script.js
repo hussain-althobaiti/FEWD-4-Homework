c552189706d4054e5484e31d018b1f3cd4ce5135
hussain-althobaiti <1@hussain-althobaiti.sa>
2019-11-01 18:42:04 +0300

    H.W-4

---
+++ HussainAlthobaiti/week4/script.js
@@ -0,0 +1,22 @@
+
+function calculate(){
+    var x;
+    x=Number(document.getElementById("input").value);
+        if (x%3==0){
+            console.log("Pling");
+            x="Pling";
+            
+        }
+        else if(x%5==0){
+            console.log("Plang");
+            x="Plang";
+        }
+        else if(x%7==0){
+            console.log("Plong");
+            x="Plong";
+        }
+        document.getElementById("output").value= x;
+    }
+    
+    
+
